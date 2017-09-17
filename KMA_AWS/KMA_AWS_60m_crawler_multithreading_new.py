c40bd2293cb0ee61d72265575344a00772d87d03
Kiehyun Kevin Park <guitar79@naver.com>
2017-09-17 18:14:12 +0900

    Add files via upload

--- KMA_AWS/KMA_AWS_60m_crawler_multithreading_new.py
+++ KMA_AWS/KMA_AWS_60m_crawler_multithreading_new.py
@@ -1,6 +1,6 @@
 """
 @author: guitar79@naver.com, yyyyy@snu.ac.kr
-view-source:http://www.kma.go.kr/cgi-bin/aws/nph-aws_txt_min?2008262022&0&MINDB_01M&0&a
+view-source:http://www.kma.go.kr/cgi-bin/aws/nph-aws_txt_min?201008262022&0&MINDB_01M&0&a
 www.kma.go.kr/cgi-bin/aws/nph-aws_txt_min?201708270810&0&MINDB_10M&0&m
 """
 from urllib.request import urlopen
@@ -18,18 +18,19 @@ prefix = 'AWS-60min'
 
 #single thread class
 class crawler():
-	def __init__(self, year, month, day, hour, minute):
+	def __init__(self, year, month, day, hour, minute, threadno):
 		self.year = year
 		self.month = month
 		self.day = day
 		self.hour = hour
 		self.minute = minute
 		self.output = ''
+		self.threadno = threadno
 
 	def fetch(self):
-		my_file = Path(prefix + '_%d%02d%02d%02d%02d.csv' % (self.year, self.month, self.day, self.hour, self.minute))
+		my_file = Path('%d/%s_%d%02d%02d%02d%02d.csv' % (self.year, prefix, self.year, self.month, self.day, self.hour, self.minute))
 		if my_file.is_file(): # csv file already exists in my folder
-			print ('File exists ' + prefix + '_%d%02d%02d%02d%02d.csv' % (self.year, self.month, self.day, self.hour, self.minute))
+			print ('File exists %d/%s_%d%02d%02d%02d%02d.csv' % (self.year, prefix, self.year, self.month, self.day, self.hour, self.minute))
 		else:	
 			while True:
 				try:
@@ -47,11 +48,12 @@ class crawler():
 						#csv delimeter
 						self.output += '\n'
 					#open output file
-					with open(prefix + '_%d%02d%02d%02d%02d.csv' % (self.year, self.month, self.day, self.hour, self.minute), 'w') as f:
+					with open('%d/%s_%d%02d%02d%02d%02d.csv' % (self.year, prefix, self.year, self.month, self.day, self.hour, self.minute), 'w') as f:
 						#write
 						f.write(self.output)
 					break
 				except:
+					sys.stderr.write("Thread #%d failed...retry\n" % self.threadno)
 					pass
 
 #crawler for single month
@@ -67,15 +69,15 @@ class crawler_month(threading.Thread):
 	def run(self):
 		for Ho in range(0,24):
 			for Mn in range(0,60,60): 
-				fetcher = crawler(self.year, self.month, self.day, Ho, Mn)
+				fetcher = crawler(self.year, self.month, self.day, Ho, Mn, self.threadno)
 				fetcher.fetch()
 				sys.stderr.write('Thread #%d - fetched %d-%02d-%02d %02d:%02d...\n' % (self.threadno, self.year, self.month, self.day, Ho, Mn))
 
 
 threadno = 0
-for year in range(2015,2016):
+for year in range(2007,2008):
 	for Mo in range(1,13):
 		for Da in range(1,32):
 			cmonth = crawler_month(year, Mo, Da, threadno)
 			cmonth.start()
-		threadno += 1
+			threadno += 1
